7e411560ab0dffffcf2eb36485683c8acca9dc58
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-10-16 16:33:34 +0000

    second site

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Create a Hello HBNB page that runs on 0.0.0.0:5000"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """the hello_hbnb function"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """just display hbnb"""
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
